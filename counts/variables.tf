f7aec8218b0bf3d890ce042b8d6a6e0ad4acbe38
Rajanikanth Kore <rajanikanthdevops2024@gmail.com>
2024-08-02 23:23:11 +0530

    Terraform Counts

---
+++ counts/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_names" {
+  type = list
+  default = ["mongodb","redis","mysql","rabbitmq","catalogue","user","cart","shipping","payment","dispatch","web"]
+}
+
+variable "ami_id" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "zone_id" {
+  default = "Z055248914HEBT8QV119S"
+}
+
+variable "domain_name" {
+  default = "devopsrk.cloud"
+}
