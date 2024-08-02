64131a78ae4072fb4d891c1b5036e4a3df8f81e6
Rajanikanth Kore <rajanikanthdevops2024@gmail.com>
2024-08-02 23:58:46 +0530

    Terraform Locals

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "zone_id" {
+  default = "Z055248914HEBT8QV119S"
+}
+
+variable "domain_name" {
+  default = "devopsrk.cloud"
+}
