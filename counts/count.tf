8507db070b8f91ae2bb3049d917fc4fdc608188e
Rajanikanth Kore <rajanikanthdevops2024@gmail.com>
2024-08-06 15:24:50 +0530

    terraform counts

--- counts/count.tf
+++ counts/count.tf
@@ -9,10 +9,10 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_route53_record" "www" {
-    count = length(var.instance_names)
-    zone_id = var.zone_id
-    name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
-    type    = "A"
-    ttl     = 1
-    records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
+  count   = length(var.instance_names)
+  zone_id = var.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
+  type    = "A"
+  ttl     = 1
+  records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 }
